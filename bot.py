d128f6d06a1646ae5dca45f953ca3c3338e2e40b
SaraSanchez <sara_sanchez82221@elpoli.edu.co>
2024-12-14 14:14:27 -0500

    First action to find element

--- bot.py
+++ bot.py
@@ -5,3 +5,20 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys 
 import time
+
+
+service = Service("driver/chromedriver.exe")
+bot = webdriver.Chrome(service=service)
+bot.maximize_window()
+bot.get("https://www.viajesexito.com")
+
+try:
+    closeSpam = bot.find_element(By.XPATH, '/html/body/div/div/div/div[1]')
+    time.sleep(2)
+    closeSpam.click()
+    time.sleep(2)
+except Exception as e:
+    print(f"Ocurrió un error: {e}")
+
+
+
